a1a23757c88a83764a4ddf9fb5a0b2ad8e385683
mrmikemcguire <moonshotmike@gmail.com>
2018-12-22 11:57:56 -0700

    finish title section

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@ $(document).ready(function()
     {
     $('#slides').superslides({
         animation: 'fade',
-        play: 5000
-    })
+        play: 5000,         //delay before automatic switching
+        pagination: false   //removes buttons at bottom
+        });
+
+    var typed = new Typed(".typed", {
+        strings: ["Wannabe Coder", "Chess Enthusiast", "Amatuer Photographer"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false
+        });
+
     });
 
