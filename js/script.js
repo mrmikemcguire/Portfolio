cbcb2c33361eac76520434ec7b7bc2525a8647bf
Mike McGuire <MoonshotMike@gmail.com>
2018-12-30 17:05:27 -0700

    styling done

--- js/script.js
+++ js/script.js
@@ -113,5 +113,25 @@ $(document).ready(function()
         return false;
         });
 
+    const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation ()
+        {
+        var body = $("body");
+        if($(window).scrollTop() >= navTop)
+            {
+            body.css("padding-top", nav.outerHeight() + "px");
+            body.addClass("fixedNav");
+            }
+        else
+            {
+            body.css("padding-top", 0);
+            body.removeClass("fixedNav");
+            }
+        }
+
 });
 
