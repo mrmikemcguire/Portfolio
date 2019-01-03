ee03b8fd0b29dd3b73e6b86b41eeeef65179a4fc
Mike McGuire <MoonshotMike@gmail.com>
2019-01-03 13:03:31 -0700

    add photos and organize files

--- js/script.js
+++ js/script.js
@@ -22,16 +22,16 @@ $(document).ready(function()
     {
     $('#slides').superslides({
         animation: 'fade',
-        play: 5000,         //delay before automatic switching
+        play: 3000,         //delay before automatic switching
         pagination: false   //removes buttons at bottom
         });
 
     var typed = new Typed(".typed", {
-        strings: ["Family Man","Computer Science Teacher", "Chess Enthusiast",
-         "Amatuer Photographer", "Spartan"],
-        typeSpeed: 70,
+        strings: ["Husband", "Father", "Teacher", "Hiker", "Cyclist", "Mustang", "Photographer", "Chessplayer",
+         "Yogi", "Spartan"],
+        typeSpeed: 140,
         loop: true,
-        startDelay: 1000,
+        startDelay: 2000,
         showCursor: false
         });
 
